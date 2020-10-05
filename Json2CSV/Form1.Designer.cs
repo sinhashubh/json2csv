f87b4b3cc605623a6398e798265a38600de2789c
shubham sinha <shubham.sinha@music-group.com>
2020-10-06 00:00:22 +0530

    Initial Commit

---
+++ Json2CSV/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Json2CSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(502, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Write File location here";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(76, 124);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(281, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Converted FileName";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(498, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(76, 249);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(630, 189);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 734);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Json2CSV/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using CsvHelper;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace Json2CSV
+{
+    public partial class Form1 : Form
+    {
+        DataTable dw = new DataTable();
+        List<JObject> job = new List<JObject>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filelocation =  textBox1.Text;
+                string exportfilename =  textBox2.Text;
+                using (StreamReader r = new StreamReader(filelocation))
+                {
+                    string json = r.ReadToEnd();
+                    string tolocation ="";
+                    File.WriteAllText(tolocation + exportfilename, jsonToCSV(json, ","));
+                }
+                label1.Text = "Converted";
+            }
+            catch (Exception r)
+            {
+                label1.Text = r.ToString();
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public DataTable jsonStringToTable(string jsonContent)
+        {
+            JObject stuff = JObject.Parse(jsonContent);
+            int i = 0;
+
+            recurse(i, jsonContent);
+            
+            foreach (JObject r in job)
+            {
+                DataRow dr = dw.NewRow();
+
+                foreach (var k in r)
+                {
+                    dr[k.Key] = k.Value;
+                }
+                dw.Rows.Add(dr);
+            }
+
+            return dw;
+        }
+
+        public JObject recur(JObject j, JObject newjo, string innerkey, int i)
+        {
+            IDictionary<string, JToken> Jsondata = j;
+            foreach (KeyValuePair<string, JToken> element in Jsondata)
+            {
+                string innerKey = innerkey + element.Key;
+                if (element.Value is JArray)
+                {
+                    recur(element.Value as JArray, innerKey, i);
+                }
+                else if (element.Value is JObject)
+                {
+                    newjo = recur(element.Value as JObject, newjo, innerKey, i);
+                }
+                else
+                {
+
+                    newjo.Add(innerKey, element.Value);
+                    if (i == 0)
+                    {
+                        dw.Columns.Add(innerKey);
+                    }
+                }
+            }
+            return newjo;
+        }
+        public void recur(JArray j, string innerkey, int i)
+        {
+            int jarr0 = 0;
+            int k = i;
+            foreach (var jo in j)
+            {
+
+                
+                    if (jarr0 == 0)
+                        k = i;
+                    else k = 2;
+                    if (jo is JArray)
+                    {
+                        recur(jo as JArray, innerkey, k);
+                    }
+                    else if (jo is JObject)
+                    {
+                        JObject newjo = new JObject();
+                        newjo = recur(jo as JObject, newjo, innerkey, k);
+                        job.Add(newjo);
+                    }
+                    else
+                    {
+                    }
+                    jarr0++;
+                
+            }
+        }
+        public void recurse( int i, string key)
+        {
+
+            JObject newjo = new JObject();
+            IDictionary<string, JToken> Jsondata = JObject.Parse(key);
+            foreach (KeyValuePair<string, JToken> element in Jsondata)
+            {
+                string innerKey = element.Key;
+                if (element.Value is JArray)
+                {
+                    recur(element.Value as JArray, innerKey, i);
+                }
+                else if (element.Value is JObject)
+                {
+                    recur(element.Value as JObject, newjo, innerKey, i);
+                }
+            }
+        }
+        public string jsonToCSV(string jsonContent, string delimiter)
+        {
+            StringWriter csvString = new StringWriter();
+            using (var csv = new CsvWriter(csvString))
+            {
+                csv.Configuration.Delimiter = delimiter;
+
+                using (var dt = jsonStringToTable(jsonContent))
+                {
+                    foreach (DataColumn column in dt.Columns)
+                    {
+                        csv.WriteField(column.ColumnName);
+                    }
+                    csv.NextRecord();
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        for (var i = 0; i < dt.Columns.Count; i++)
+                        {
+                            csv.WriteField(row[i]);
+                        }
+                        csv.NextRecord();
+                    }
+                }
+            }
+            return csvString.ToString();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
